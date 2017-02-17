7692591f67bbca4b82c7bab5fd1166d779f66f3c
David Thefan <david.thefan@gmail.com>
2017-02-17 07:23:36 +0000

    changed scope of generated data

--- snippet-random-letter-generator.py
+++ snippet-random-letter-generator.py
@@ -2,9 +2,8 @@ import string
 import random
 
 
-def runegen (size=1, chars=string.ascii_uppercase + string.digits):
+def runegen (size=1, chars=string.ascii_lowercase + string.ascii_uppercase):
     return ''.join(random.choice(chars) for _ in range(size))
-
 print (runegen())
 
 ''' code to randomly generate a character, using Runes font, this will
