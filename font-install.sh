ab80986a105a2a1f03b537fa642980dc99cfc23f
David Thefan <david.thefan@gmail.com>
2017-02-20 02:56:04 +0000

    font install script

--- font-install.sh
+++ font-install.sh
@@ -1,7 +1,29 @@
 #bin/bash
+echo "Installing Truetype font support"
+sudo apt-get install ttf-mscorefonts-installer
+
+echo "Downloading font zip, and extracting"
 wget http://dl.1001fonts.com/runes.zip && unzip runes.zip
-sudo mv Runes.ttf /usr/share/fonts
+echo
+echo
+echo "Creating /usr/share/fonts/truetype/runes"
+sudo mkdir /usr/share/fonts/truetype/runes
+
+echo "Moving Runes.ttf and Readme to /usr/share/fonts/truetype/runes"
+sudo mv Runes.ttf /usr/share/fonts/truetype/runes
+sudo mv 1001* /usr/share/fonts/runes
+
+echo "Cleaning up..."
 rm runes.zip
+
+echo "Updating font cache"
+sudo fc-cache -fv
+
+echo
+echo "Complete."
+
+
+
 
 
 
